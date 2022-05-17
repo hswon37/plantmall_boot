dcd6145ace19bfa7543aa4b3c05105e447c2981c
moongsilismine <hsjhyok@naver.com>
2022-05-17 21:58:34 +0900

    sewon productDetail.html js edit(function test fin at local)

--- src/main/java/com/example/plantmall/controller/productController.java
+++ src/main/java/com/example/plantmall/controller/productController.java
@@ -8,41 +8,41 @@ public class productController {
 	//제품 목록
 	@RequestMapping("/product/list")
 	public String showProductList() {
-		return "productList";
+		return "product/productList";
 	}
 	//제품 상세
 	@RequestMapping("/product/detail")
 	public String showProductDetail() {
-		return "productDetail";
+		return "product/productDetail";
 	}
 	//제품 관리 목록
 	@RequestMapping("/product/manage")
 	public String showProductManage() {
-		return "productManage";
+		return "product/productManage";
 	}
 	//제품 등록
 	@RequestMapping("/product/registration")
 	public String showProductRegistration() {
-		return "productRegistration";
+		return "product/productRegistration";
 	}
 	//제품 수정
 	@RequestMapping("/product/modify")
 	public String showProductModify() {
-		return "productModify";
+		return "product/productModify";
 	}
 	//제품 삭제
 	@RequestMapping("/product/delete")
 	public String showProductDelete() {
-		return "productDelete";
+		return "product/productDelete";
 	}
 	//제품 검색
 	@RequestMapping("/product/search")
 	public String showProductSearch() {
-		return "productSearch";
+		return "product/productSearch";
 	}
 	//제품 검색 결과
 	@RequestMapping("/product/searchResult")
 	public String showProductSearchResult() {
-		return "searchResult";
+		return "product/searchResult";
 	}
 }
