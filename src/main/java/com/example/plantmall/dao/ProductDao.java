0e91ae8d0263fd80fa022b3af4379220dc2d0e03
mohyerolo <garam2101@naver.com>
2022-05-30 18:53:07 +0900

    review에 필요한 product관련 업데이트

--- src/main/java/com/example/plantmall/dao/ProductDao.java
+++ src/main/java/com/example/plantmall/dao/ProductDao.java
@@ -6,6 +6,7 @@ import org.springframework.dao.DataAccessException;
 
 import com.example.plantmall.controller.SearchValueCommand;
 import com.example.plantmall.domain.Product;
+import com.example.plantmall.domain.Review;
 
 public interface ProductDao {
 
@@ -24,4 +25,9 @@ public interface ProductDao {
 	void updateProduct(Product product) throws DataAccessException;
 	
 	void deleteProduct(Product product) throws DataAccessException;
+	
+	// 제품과 리뷰 다 가져오기
+	Product getProductAndReviewList(String productId) throws DataAccessException;
+	// 제품 리뷰 정보
+	List<Review> getReviewsByProductId(String productId) throws DataAccessException;
 }
